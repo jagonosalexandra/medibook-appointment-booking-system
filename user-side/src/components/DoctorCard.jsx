ea72f291c38beef523de224104203218728465ea
Alexandra Jagonos <jagonosalexandra@gmail.com>
2026-03-11 20:46:25 +0800

    Create doctor card component

---
+++ user-side/src/components/DoctorCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Button from './Button'
+
+const DoctorCard = ({ name, photoUrl, department }) => {
+  return (
+    <div className='flex flex-col gap-3.5 pb-6 rounded-xl bg-card border border-gray-300 contain-content overflow-hidden'>
+        <img className='w-full aspect-square object-cover object-top' src={photoUrl} alt={name} />
+        
+        <div className='px-4 text-lg font-bold text-primary'>
+            {name}
+            <span className='block text-sm font-light text-gray-500'>{department}</span>
+        </div>
+        
+        <div className='flex items-center gap-4 px-4'>
+            <Button
+                label="Profile"
+                variant="secondary"
+                fullWidth
+            />
+            <Button
+                label="Book Now"
+                variant="primary"
+                fullWidth
+            />
+        </div>
+    </div>
+  )
+}
+
+export default DoctorCard
