d54fec7c25e98108383d4e49b4ce3fa43bf93e55
Alexandra Jagonos <jagonosalexandra@gmail.com>
2026-03-17 10:53:09 +0800

    create api service

---
+++ user-side/src/services/api.js
@@ -0,0 +1,11 @@
+// user-side/src/services/api.js
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+export default api
