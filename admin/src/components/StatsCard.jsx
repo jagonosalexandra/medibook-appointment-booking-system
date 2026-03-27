51ca96f22c020ecbcb04ca836f722233a834d359
Alexandra Jagonos <jagonosalexandra@gmail.com>
2026-03-27 17:12:44 +0800

    style: change width

--- admin/src/components/StatsCard.jsx
+++ admin/src/components/StatsCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const StatsCard = ({ name, count, icon, color }) => {
   return (
-    <div className='flex flex-col gap-4 w-85 p-4 relative bg-card border border-gray-200 rounded-xl shadow-lg'>
+    <div className='flex flex-col gap-4 w-70 p-4 relative bg-card border border-gray-200 rounded-xl shadow-lg'>
         <p className='text-sm text-gray-600'>{name}</p>
         <p className='text-4xl font-bold'>{count}</p>
         <img 
