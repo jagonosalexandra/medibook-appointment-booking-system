7669dc74c1a82bd1af2e3c41bb5ca894cc076b40
Alexandra Jagonos <jagonosalexandra@gmail.com>
2026-04-10 18:08:17 +0800

    add file type validation and destination

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,11 +1,25 @@
 import multer from "multer"
 
 const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, '/temp')
+    },
     filename: function (req, file, callback) {
         callback(null, file.originalname)
     }
 })
 
-const upload = multer({ storage })
+const upload = multer({ 
+    storage,
+    limits: {fileSize: 5 * 1024 * 1024},
+    fileFilter: (req, file, callback) => {
+        const allowed = ['image/jpeg', 'image/png', 'image/jpg', 'image/webp']
+        if (allowed.includes(file.mimetype)) {
+            callback(null, true)
+        } else {
+            callback(new Error('Only JPEG, JPG, PNG and WEBP formats are allowed!'))
+        }
+    }
+})
 
 export default upload
