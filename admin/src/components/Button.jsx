26f0d2e38f997bf1ab40704f960d487571189257
Alexandra Jagonos <jagonosalexandra@gmail.com>
2026-04-10 20:58:00 +0800

    fix disabled variant

--- admin/src/components/Button.jsx
+++ admin/src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Button = ({ label, variant, onClick, fullWidth = false, type = "button", disabled = false }) => {
-  
+
     const variants = {
         primary: "bg-primary text-white hover:bg-primary-dark",
         secondary: "bg-white border border-primary text-primary hover:bg-gray-50",
@@ -9,20 +9,21 @@ const Button = ({ label, variant, onClick, fullWidth = false, type = "button", d
         disabled: "bg-gray-200 text-gray-500 cursor-not-allowed"
     }
 
+    const isDisabled = disabled || variant === 'disabled'
+    const isFullWidth = fullWidth ? "w-full" : "w-auto"
     const selectedVariant = variants[variant]
 
-    const isFullWidth = fullWidth ? "w-full" : "w-auto"
-  
     return (
         <button
             type={type}
-            className={`${selectedVariant} ${isFullWidth} px-6 py-2 cursor-pointer rounded-xl font-semibold shadow-md transition-all duration-300`}
-            onClick={onClick}
-            disabled={disabled}
+            className={`${selectedVariant} ${isFullWidth} px-6 py-2 cursor-pointer rounded-xl font-semibold shadow-md transition-all duration-300
+                        disabled:cursor-not-allowed disabled:pointer-events-none`}
+            onClick={isDisabled ? undefined : onClick}
+            disabled={isDisabled}
         >
             {label}
         </button>
-  )
+    )
 }
 
 export default Button
