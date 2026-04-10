5d3f2dc982e5cd0f92fe407385a617bba7f2ba75
Alexandra Jagonos <jagonosalexandra@gmail.com>
2026-04-10 18:31:42 +0800

    mount pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,20 +1,44 @@
-import { useState } from 'react'
-import Login from './pages/Login'
 import { Navigate, Route, Routes } from 'react-router-dom'
-import ProtectedRoute from './components/ProtectedRoute'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
+import ProtectedRoute from './components/ProtectedRoute'
+import Sidebar from './components/Sidebar'
+import Appointments from './pages/Appointments'
+import AppointmentDetail from './pages/AppointmentDetail'
+import Doctors from './pages/Doctors';
+import AddDoctor from './pages/AddDoctor';
+import EditDoctorProfile from './pages/EditDoctorProfile';
+import TimeSlots from './pages/TimeSlots';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
+    <div className="bg-[#F8F9FD] min-h-screen">
+      <ToastContainer />
       <Routes>
         <Route path='/login' element={<Login />} />
-        <Route path='/dashboard' element={
+        <Route path='/*' element={
           <ProtectedRoute>
-            <Dashboard />
+            <Navbar />
+            <div className='flex items-start'>
+              <Sidebar />
+              <div className='flex-1 flex flex-col'>
+                <Routes>
+                  <Route path='/dashboard' element={<Dashboard />} />
+                  <Route path='/' element={<Navigate to='/dashboard' replace />} />
+                  <Route path='/appointments' element={<Appointments />} />
+                  <Route path='/appointment/:appId' element={<AppointmentDetail />} />
+                  <Route path='/doctors' element={<Doctors />} />
+                  <Route path='/doctor/new' element={<AddDoctor />} />
+                  <Route path='/doctor/:docId' element={<EditDoctorProfile />} />
+                  <Route path='/time-slots' element={<TimeSlots />} />
+                </Routes>
+              </div>
+            </div>
           </ProtectedRoute>
         } />
-        <Route path='/' element={<Navigate to='/dashboard' replace />} />
       </Routes>
     </div>
   )
