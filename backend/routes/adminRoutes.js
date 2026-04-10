8ee9d2cd78df5ba3d78ed94a7cfb29eb77184ad5
Alexandra Jagonos <jagonosalexandra@gmail.com>
2026-04-10 18:10:37 +0800

    add doctor fetching, adding slots, fetching slots toggling slot, and toggling doctor status

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,7 +1,19 @@
 import express from 'express';
-import { addDoctor, adminDashboard, updateDoctor } from '../controllers/adminController.js';
+import {
+    addDoctor,
+    adminDashboard,
+    updateDoctor,
+    addTimeSlots,
+    doctors,
+    getTimeSlots,
+    toggleSlotAvailability,
+    toggleDoctorActive
+} from '../controllers/adminController.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
-import { appointments, updateAppointment } from '../controllers/appointmentController.js'
+import {
+    appointments,
+    updateAppointment,
+} from '../controllers/appointmentController.js'
 import upload from '../middlewares/multer.js'
 
 const adminRouter = express.Router()
@@ -11,5 +23,10 @@ adminRouter.get('/appointments', authMiddleware, appointments)
 adminRouter.put('/update-appointment/:id', authMiddleware, updateAppointment)
 adminRouter.post('/add-doctor', authMiddleware, upload.single("image"), addDoctor)
 adminRouter.put('/update-doctor/:id', authMiddleware, upload.single("image"), updateDoctor)
+adminRouter.get('/doctors', authMiddleware, doctors)
+adminRouter.post('/add-slots', authMiddleware, addTimeSlots)
+adminRouter.get('/get-slots/:docId', authMiddleware, getTimeSlots)
+adminRouter.post('/toggle-slot', authMiddleware, toggleSlotAvailability)
+adminRouter.post('/toggle-doctor', authMiddleware, toggleDoctorActive)
 
 export default adminRouter
