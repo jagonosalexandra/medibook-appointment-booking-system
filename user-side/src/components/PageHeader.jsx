1794a974b3b2c14c33f3c469c5723b184966506a
Alexandra Jagonos <jagonosalexandra@gmail.com>
2026-06-18 20:08:49 +0800

    change padding on responsive design

--- user-side/src/components/PageHeader.jsx
+++ user-side/src/components/PageHeader.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const PageHeader = ({ title, subtitle }) => {
     return (
-        <div className='bg-secondary p-8 rounded-lg'>
+        <div className='bg-secondary px-8 md:py-16 py-8'>
             <h1 className='text-xl font-black tracking-wider text-primary-dark'>{title}</h1>
-            <p className='max-w-lg leading-relaxed'>{subtitle}</p>
+            <p className='leading-relaxed text-gray-500'>{subtitle}</p>
         </div>
     )
 }
